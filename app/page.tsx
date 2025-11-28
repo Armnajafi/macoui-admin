470be8493a2b39427ad58030117e13bc66ad1953
Armnajafi <armnajafi@gmail.com>
2025-11-28 19:51:24 +0330

    test ci cd

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function DashboardPage() {
     <DashboardLayout title="Dashboard" wave={true}>
       <div className="mx-4">
         <div className="space-y-6">
-          {/* Search Bar  test*/}
+          {/* Search Bar te*/}
           <MobileSearchBar />
 
           {/* Stats Cards */}
